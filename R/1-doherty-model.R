eb1006b604800f743ab9add029f029e5bc18cca5
Mark S. Handcock <handcock@ucla.edu>
2020-04-13 21:16:38 +0800

    Completed Doherty model specification based on Moss et al 2020. It is still a draft but passes tests.
    Enhanced plotting to allow prevalence and other plotting options.

--- R/1-doherty-model.R
+++ R/1-doherty-model.R
@@ -26,17 +26,17 @@
 #'
 #' @examples
 #' (In progress)
-#' # no self-quarantine
-#' param <- seimrqc_param(R0=2.5,lambdaimp=0,sigma1=0.3,sigma2=0.3,gamma1=0.3,gamma2=0.3,Qeff=0.5,Meff=0.99,rho=0,alpha=0.2,eta=0.01,delta=0.2,kappa=20,pm=1)
+#' # Non-Indigenous,  aged 50-59, no self-quarantine
+#' param <- seimrqc_param(R0=2.53,lambdaimp=0,sigma1=1/1.6,sigma2=1/1.6,gamma1=1/4.0,gamma2=1/5.68,gammaq1=1/4.0,gammaq2=1/5.68,Qeff=0.5,Meff=0,rho=0,eta=1/sqrt(2),alphamBeta=0.5,probHospGivenInf=0.09895,delta=1/14,kappa=20,pm=1)
 #' state0 <- seimrqc_state0(S0=100,E10=1)
 #' res <- simulate_seimrqc(t = 100,state_t0 = state0,param = param)
-#' plot(res,c("S","EI","M","Rt"))
+#' plot(res,c("S","Itotal","M","Rtotal"))
 #'
 #' # with self-quarantine
-#' param <- seimrqc_param(R0=2.5,lambdaimp=0,sigma1=0.3,sigma2=0.3,gamma1=0.3,gamma2=0.3,Qeff=0.5,Meff=0.99,rho=0.8,alpha=0.2,eta=0.01,delta=0.2,kappa=20,pm=1)
+#' param <- seimrqc_param(R0=2.5,lambdaimp=0,sigma1=0.3,sigma2=0.3,gamma1=0.3,gamma2=0.3,gammaq1=0.3,gammaq2=0.3,Qeff=0.5,Meff=0.99,rho=0.8,eta=1/sqrt(2),alphamBeta=0.5,delta=0.2,kappa=20,pm=1)
 #' state0 <- seimrqc_state0(S0=100,E10=1)
 #' res <- simulate_seimrqc(t = 100,state_t0 = state0,param = param)
-#' plot(res,c("S","EI","M","Rt"))
+#' plot(res,c("S","Itotal","M","Rtotal"))
 #'
 #' @export
 simulate_seimrqc <- function(t,state_t0,param) {
@@ -45,35 +45,39 @@ simulate_seimrqc <- function(t,state_t0,param) {
     stopifnot(class(param) == "seimrqc_param")
 
     # simulation
+    alphas <- param$alphas
     param = EpiModel::param.dcm(R0=param$R0,
                                 lambdaimp=param$lambdaimp,
                                 sigma1=param$sigma1,
                                 sigma2=param$sigma2,
                                 gamma1=param$gamma1,
                                 gamma2=param$gamma2,
+                                gammaq2=param$gammaq2,
+                                gammaq1=param$gammaq1,
                                 Qeff=param$Qeff,
                                 Meff=param$Meff,
                                 rho=param$rho,
-                                alpha=param$alpha,
                                 eta=param$eta,
+                                alpha=param$alpha,
                                 delta=param$delta,
                                 kappa=param$kappa,
                                 pm=param$pm)
+    param$alphas <- alphas
     init = EpiModel::init.dcm(S=state_t0$S,
                               E1=state_t0$E1,
                               E2=state_t0$E2,
                               I1=state_t0$I1,
                               I2=state_t0$I2,
+                              R=state_t0$R,
                               M=state_t0$M,
                               Rm=state_t0$Rm,
-                              R=state_t0$R,
                               Eq1=state_t0$Eq1,
                               Eq2=state_t0$Eq2,
                               Iq1=state_t0$Iq1,
                               Iq2=state_t0$Iq2,
+                              Rq=state_t0$Rq,
                               Mq=state_t0$Mq,
                               Rqm=state_t0$Rqm,
-                              Rq=state_t0$Rq,
                               CTm=state_t0$CTm,
                               CTnm=state_t0$CTnm)
     control = EpiModel::control.dcm(nsteps=t,
@@ -100,36 +104,45 @@ simulate_seimrqc <- function(t,state_t0,param) {
 #' Setup function
 #'
 #' @param R0 Basic reproduction number (S -> E)
-#' @param lambdaimp The force of infection from importated cases.
+#' @param lambdaimp The force of infection from imported cases.
 #' @param sigma1 Inverse of the average length of first latent period (E1 -> E2)
 #' @param sigma2 Inverse of the average length of second latent period (E2 -> I1)
 #' @param gamma1 Inverse of the average length of first infectious period (I1 -> I2 or H)
 #' @param gamma2 Inverse of the average length of second infectious period (I2 -> R)
+#' @param gammaq1 Inverse of the average length of first infectious period for quarantined cases (Iq1 -> Iq2)
+#' @param gammaq2 Inverse of the average length of second infectious period for quarantined cases (Iq2 -> Rq)
 #' @param Qeff Effect of quarantine on infectiousness, should be in (0,1)
 #' @param Meff Effect of medical management on infectiousness, should be in (0,1)
 #' @param rho the proportion of contacts (of ascertained cases) that will self-quarantine (0,1)
-#' @param alpha Proportion of infected requesting medical assistance, should be in (0,1)
-#' @param eta ...
-#' @param delta The duration of quarantine for contacts of ascertained cases (14 days)
+#' @param eta Scaling factor for hospitalization ("severe").
+#' @param alphamBeta Scaling factor for proportion of non-severe requesting medical assistance.
+#' It should be in (0,1) and in Beta draw in McVernon et al 2020).
+#' @param probHospGivenInf Probability of hospitalization given infection (typically from Table 3 in McVernon et al 2020)
+#' @param delta Inverse of the duration of quarantine for contacts of ascertained cases (14 days)
 #' @param kappa The per-person contact rate (20 people per day)
 #' @param pm The probability of presenting cases being effectively managed
 #'
 #' @return List of seimrqc model parameters
 #'
 #' @export
-seimrqc_param <- function(R0,lambdaimp,sigma1,sigma2,gamma1,gamma2,Qeff,Meff,rho,alpha,eta,delta,kappa,pm) {
+seimrqc_param <- function(R0,lambdaimp,sigma1,sigma2,gamma1,gamma2,gammaq1,gammaq2,Qeff,Meff,rho,eta,alphamBeta,probHospGivenInf,delta,kappa,pm) {
 
+    min_alpham <- 0.05+0.2*(eta-0.01)/0.99
+    max_alpham <- 0.15+0.6*(eta-0.01)/0.99
+    alpham <- min_alpham+(max_alpham-min_alpham)*alphamBeta
+    alphas <- eta * probHospGivenInf
+    alpha = alphas + alpham*(1 - alphas)
     # output with class seimrqc_param
     param = list(R0=R0,lambdaimp=lambdaimp,sigma1=sigma1,sigma2=sigma2,gamma1=gamma1,
-                 gamma2=gamma2,Qeff=Qeff,Meff=Meff,rho=rho,alpha=alpha,eta=eta,delta=delta,
-                 kappa=kappa,pm=pm)
+                 gamma2=gamma2,gammaq1=gammaq1,gammaq2=gammaq2,Qeff=Qeff,Meff=Meff,rho=rho,eta=eta,alpha=alpha,
+                 alphas=alphas,delta=delta,kappa=kappa,pm=pm)
     class(param) = "seimrqc_param"
 
     # return
     param
 }
 
-#' SEIMR-QC model inital state
+#' SEIMR-QC model initial state
 #'
 #' Setup function
 #'
@@ -152,12 +165,15 @@ seimrqc_param <- function(R0,lambdaimp,sigma1,sigma2,gamma1,gamma2,Qeff,Meff,rho
 #' @return List of seimrqc model initial states
 #'
 #' @export
-seimrqc_state0 <- function(S0,E10,E20=0,I10=0,I20=0,M0=0,Rm0=0,R0=0,Eq10=0,Eq20=0,Iq10=0,Iq20=0,Mq0=0,Rqm0=0,Rq0=0,CTm0=0) {
+seimrqc_state0 <- function(S0,E10,E20=0,I10=0,I20=0,R0=0,M0=0,Rm0=0,Eq10=0,Eq20=0,Iq10=0,Iq20=0,Rq0=0,Mq0=0,Rqm0=0,
+                           CTm0=2,CTnm0=20){
     # assertions
     stopifnot(E10 >= 1)
 
     # output with class seimrqc_state0
-    state0 = list(S=S0,E1=E10,E2=E20,I1=I10,I2=I20,M=M0,Rm=Rm0,R=R0,Iq1=Iq10,Iq2=Iq20,Eq1=Eq10,Eq2=Eq20,Mq=Mq0,Rqm=Rqm0,Rq=Rq0,CTm=CTm0,CTnm=0)
+    state0 = list(S=S0,E1=E10,E2=E20,I1=I10,I2=I20,R=R0,M=M0,Rm=Rm0,Eq1=Eq10,Eq2=Eq20,Iq1=Iq10,Iq2=Iq20,Rq=Rq0,Mq=Mq0,Rqm=Rqm0,
+                  CTm=CTm0,CTnm=CTnm0,
+                  N=S0 + E10 + E20 + I10 + I20 + R0 + M0 + Rm0 + Eq10 + Eq20 + Iq10 + Iq20 + Rq0 + Mq0 + Rqm0)
     class(state0) = "seimrqc_state0"
 
     # return
@@ -168,7 +184,7 @@ seimrqc_state0 <- function(S0,E10,E20=0,I10=0,I20=0,M0=0,Rm0=0,R0=0,Eq10=0,Eq20=
 #'
 #' @param t Number of time steps over which to sample from the model
 #' @param state_t0 Initial state (S,E1,E2,I1,I2,R,M,Rm,Eq1,Eq2,Iq1,Iq2,Rq,Mq,Rqm)
-#' @param param The model parameters (R0,sigma1,sigma2,gamma1,gamma2,Qeff,Heff,rho,alpha,eta)
+#' @param param The model parameters (R0,sigma1,sigma2,gamma1,gamma2,gammaq1,gammaq2,Qeff,Heff,rho,alpha)
 #'
 #' @return derivatives of SEIMR-QC model states with respect to time
 #'
@@ -179,26 +195,21 @@ seimrqc_model <- function(t,state_t0,param) {
         N <- S + E1 + E2 + I1 + I2 + R + M + Rm + Eq1 + Eq2 + Iq1 + Iq2 + Rq + Mq + Rqm
 
         # derived parameters
-        #alpha = eta + alpham*(1 - eta)
-        alpha = alpha
         beta = R0*(1/((1/sigma2) + (1/gamma1) + (1/gamma2)))
         betamq = beta*(1 - max(Meff,Qeff))
         lambda = lambdaimp + beta*(E2 + I1 + I2) + beta*(1 - Qeff)*(Eq2 + Iq1 + Iq2) +
             beta*(1 - Meff)*M + betamq*Mq
 
-        thetam = (S/N)*(CTm/N)
-        # thetam = (S/N)*(CTm/(CTm + CTnm))
-        # thetanm = (S/N)*(CTnm/(CTm + CTnm))
+        Thetam =  (S/N)*( CTm/(CTm + CTnm))
+        Thetanm = (S/N)*(CTnm/(CTm + CTnm))
 
         # Differential equations
         # contacts of cases
-        # dCTm = kappa*(gamma1*I1 + gammaq1*Iq1)*(alpha*pm) - (1/delta)*CTm - lambda*thetam
-        # dCTnm = kappa*(gamma1*I1 + gammaq1*Iq1)*(1 - alpha*pm) - (1/delta)*CTnm - lambda*thetanm
-        dCTm = kappa*(gamma1*I1 + gamma1*Iq1)*(alpha*pm) - delta*CTm - lambda*thetam
-        dCTnm = 0
+        dCTm =  kappa*(gamma1*I1 + gammaq1*Iq1)*(alpha*pm) - delta*CTm - lambda*Thetam
+        dCTnm = kappa*(gamma1*I1 + gammaq1*Iq1)*(1 - alpha*pm) - delta*CTnm - lambda*Thetanm
         # general population
         dS = -(S/N)*lambda
-        dE1 = lambda*(S/N)*(1 - rho*thetam) - sigma1*E1
+        dE1 = lambda*(S/N - rho*Thetam) - sigma1*E1
         dE2 = sigma1*E1 - sigma2*E2
         dI1 = sigma2*E2 - gamma1*I1
         dI2 = gamma1*I1*(1 - alpha*pm) - gamma2*I2
@@ -206,19 +217,27 @@ seimrqc_model <- function(t,state_t0,param) {
         dR = gamma2*I2
         dRm = gamma2*M
         # quarantined population
-        dEq1 = lambda*(S/N)*rho*thetam - sigma1*Eq1
+        dEq1 = lambda*rho*Thetam - sigma1*Eq1
         dEq2 = sigma1*Eq1 - sigma2*Eq2
-        dIq1 = sigma2*Eq2 - gamma1*Iq1
-        dIq2 = gamma1*Iq1*(1 - alpha*pm) - gamma2*Iq2
-        dMq = gamma1*Iq1*alpha*pm - gamma2*Mq
-        dRq = gamma2*Iq2
-        dRqm = gamma2*Mq
+        dIq1 = sigma2*Eq2 - gammaq1*Iq1
+        dIq2 = gammaq1*Iq1*(1 - alpha*pm) - gammaq2*Iq2
+        dMq = gammaq1*Iq1*alpha*pm - gammaq2*Mq
+        dRq = gammaq2*Iq2
+        dRqm = gammaq2*Mq
+
+# cat(sprintf("t = %f dM %f gamma1*alpha*pm %f gamma2 %f I1 %f M %f alpha %f Rtotal %f E2+I1+I2 %f Eq2+Iq1+Iq2 %f CTm %f CTnm %f lambda %f\n",t,dM,gamma1*alpha*pm,gamma2,I1,M,alpha,R+Rm+Rq+Rqm,E2+I1+I2,Eq2+Iq1+Iq2,CTm,CTnm,lambda))
 
         # return
         list(c(dS,dE1,dE2,dI1,dI2,dR,dM,dRm,dEq1,dEq2,dIq1,dIq2,dRq,dMq,dRqm,dCTm,dCTnm),
-             EI=E1+Eq1+E2+Eq2+I1+Iq1+I2+Iq2,
-             Mt=M+Mq,
-             Rt=R+Rm+Rq+Rqm)
+             Itotal=E1+Eq1+E2+Eq2+I1+Iq1+I2+Iq2,
+             I=E1+E2+I1+I2,
+             Ictotal=E1+E2+I1+I2,
+             Mtotal=M+Mq,
+             Rtotal=R+Rm+Rq+Rqm,
+             H=alphas*(E1+E2)+Eq1+Eq2,
+             D=(alphas*(E1+E2)+Eq1+Eq2)*0.29335*0.5, # hospitalized that require ICU and 50% die in ICU
+             Q=Eq1+Eq2+Iq1+Iq2+Mq, # Number in quanteen
+             N=S + E1 + E2 + I1 + I2 + R + M + Rm + Eq1 + Eq2 + Iq1 + Iq2 + Rq + Mq + Rqm)
     })
 }
 
--- R/2-plot.R
+++ R/2-plot.R
@@ -13,15 +13,36 @@ dcm2df <- function(mod) {
 #'
 #' @param x model
 #' @param y compartments to plot, a character vector
+#' @param popfrac If \code{TRUE}, plot prevalence of values rather than numbers
 #' @param ... see ggplot2::qplot
 #'
 #' @method plot covoidd
 #'
 #' @export
-plot.covoidd <- function(x,y,...) {
+plot.covoidd <- function(x,y,popfrac=FALSE,cumulative=FALSE,main="Doherty model",...) {
+#   yn = unique(c("N",y))
     df = dcm2df(x)
     df = df[df$compartment %in% y,]
-    p = ggplot2::ggplot(df) +
+    if(cumulative){
+     df$number <- cumsum(df$number)
+    }
+    if(popfrac){
+     df$number <- df$number / x$epi[["N"]][1]
+     if(cumulative){
+       ylab <- "Cumulative proportion of the population"
+     }else{
+       ylab <- "Proportion of the population"
+     }
+    }else{
+     if(cumulative){
+       ylab <- "Cumulative number of people"
+     }else{
+       ylab <- "Number of people"
+     }
+    }
+    xlab <- "time since outbreak (days)"
+    p = ggplot2::ggplot(df) + ggplot2::labs(y=ylab, y=xlab) +
+        ggplot2::ggtitle(main) +
         ggplot2::geom_line(ggplot2::aes(x = t, y = number, col = compartment)) +
         ggplot2::theme_bw()
     p
